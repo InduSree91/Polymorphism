057e758c9280a7f07124aaae589f3528915c6092
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-13 14:53:20 +0530

    Add project files.

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+// Method Overloading
+namespace Polymorphism
+{
+    class Program
+    {
+        public void Add()
+        {   
+            int a = 12;
+            int b = 13;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+
+        public void Add(string a, string b)
+        {
+            string c = a + b;
+            Console.WriteLine(c);
+        }
+
+        public void Add(float a, float b)
+        {
+            float c = a + b;
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+                        
+            p.Add();
+            p.Add(2, 4);
+            p.Add("Hello", " World!");
+            p.Add(2.34F, 9.36F);
+        }
+    }
+}
